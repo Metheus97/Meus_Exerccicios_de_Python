28853cfaff7d20738c23e0c1788d993b40f0f33e
Matheus Pereira <78231377+Metheus97@users.noreply.github.com>
2021-07-06 20:52:14 -0300

    ex97 def

---
+++ ex97.py
@@ -0,0 +1,12 @@
+
+def escreva(msg):
+    tam = len(msg) + 4
+    print('~' * tam)
+    print(f'  {msg}')
+    print('~' * tam)
+
+
+#Programa principal
+escreva('Matheus é lindo')
+escreva('Forte')
+escreva('Olá galera!')
