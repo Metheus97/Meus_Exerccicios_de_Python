7d650ae032482e751fd6be967620801b31c18f27
Matheus Pereira <78231377+Metheus97@users.noreply.github.com>
2021-07-26 22:06:20 -0300

    ex106

--- ex106.py
+++ ex106.py
@@ -1,18 +1,39 @@
-def ajuda(param):
-    from time import sleep
-    print(f'\033[0;3;44m~' * 50, f'\n Documentação para a função {param}\n', '~' * 50)
-    print('\033[m', flush=True)
-    sleep(1.2)
-    return help(param)
+from time import sleep
+
+paleta = ['\033[m',  # 0- Sem cor
+          '\033[41m',  # 1- Vermelho
+          '\033[42m',  # 2- Verde
+          '\033[43m',  # 3- Amarelo
+          '\033[44m',  # 4- Azul
+          '\033[45m',  # 5- Roxo
+          '\033[46m',  # 6- Branco
+          ]
+
+
+def titulo(msg, cor=0):
+    tam = len(msg) + 4
+    print(paleta[cor], end='')
+    print('-+' * tam)
+    print(f'  {msg}')
+    print('-+' * tam)
+    print(paleta[0], end='')
+    sleep(1)
+
+
+def ajuda(com):
+    titulo(f'Manual da função \'{com}\'', 4)
+    print(paleta[3], end='')
+    help(com)
+    sleep(1)
 
 
 # Programa principal
+comando = ''
 while True:
-    print(f'\033[0;3;42m~' * 30, '\n  SISTEMA DE AJUDA PYTHON\n', '~' * 30)
-    print('\033[m')
-    fun = str(input('Função ou Biblioteca > ')).lower()
-    if fun == 'fim':
+    titulo('SISTEMA PyHELP', 2)
+    comando = str(input('Função Biblioteca >'))
+    if comando.upper() == 'FIM':
         break
-    ajuda(fun)
-print(f'\033[0;3;41m~' * 30, '\n  SISTEMA FINALIZADO!\n', '~' * 30)
-print('\033[m')
+    else:
+        ajuda(comando)
+titulo('SISTEMA FINALIZADO', 1)
