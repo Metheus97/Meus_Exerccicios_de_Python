1c1fc058e0da89161be23d5ad07da40b9217a25e
Matheus Pereira <78231377+Metheus97@users.noreply.github.com>
2021-08-03 20:47:08 -0300

    ex115

--- ex115/Sistema.py
+++ ex115/Sistema.py
@@ -1,15 +1,25 @@
 from ex115.lib.interface import *
+from ex115.lib.arquivo import *
 from time import sleep
+
+arq = 'cadastro.txt'
+
+if not arqExiste(arq):
+    criarArquivo(arq)
 
 while True:
     açao = menu(['Cadastrar pessoas', 'Ver lista de pessoas', 'Sair do programa'])
     if açao == 1:
-        cabeçalho('Cadastrar pessoas')
+        #cadastrar nova pessoa
+        nome = str(input('Nome: '))
+        idade = leiaint('Idade: ')
+        cadastroPessoa(arq, nome, idade)
     elif açao == 2:
-        cabeçalho('Ver lista de pessoas')
+        # listar conteudo
+        lerArquivo(arq)
     elif açao == 3:
         cabeçalho('Sair do programa... Até logo!')
         break
     else:
         print('\033[31mERRO! Comando invalido.\033[m')
-    sleep(1.5)
+    sleep(2.5)
